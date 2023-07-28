c65ce82e2dd0d4557e538b19b47d238e6f996d03
alimmondal <italimbd@gmail.com>
2023-07-28 18:52:35 +0600

    upd

---
+++ src/app/not-found.js
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1 className="text-6xl">NOt found page!</h1>
+    </div>
+  );
+};
+
+export default NotFound;
